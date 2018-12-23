573548e91963a00999987222a4ba8f8fa3e4b0c8
colourdelete <colourdelete@gmail.com>
2018-12-23 14:21:33 -0500

    Change destination dir to ~/Apps.

--- copy.py
+++ copy.py
@@ -13,7 +13,7 @@ class copy():
         app_data_file = open(os.path.join(self.unpacker_result_path, 'app_data.json'))
         app_data = json.load(app_data_file)
         app_data_file.close()
-        self.destination_path = '/home/kenxshibata/apps/{id}/{edition}/{ver}/{channel}/{language0}-{language1}'.format(id=app_data['id'], ver=app_data['ver'], channel=app_data['channel'], edition=app_data['edition'], language0=app_data['language'][0], language1=app_data['language'][1])
+        self.destination_path = '/home/kenxshibata/Apps/{id}/{edition}/{ver}/{channel}/{language0}-{language1}'.format(id=app_data['id'], ver=app_data['ver'], channel=app_data['channel'], edition=app_data['edition'], language0=app_data['language'][0], language1=app_data['language'][1])
     def _copy(self):
         print('Info  Copying from {} to {}...'.format(self.unpacker_result_path, self.destination_path))
         try:
