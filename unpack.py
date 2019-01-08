814e052a68faee194595b31d7680e416e4e92b9b
colourdelete <colourdelete@gmail.com>
2019-01-08 02:44:53 +0000

    GUI Support 7.8 - All Bugfixed! (only CLI, but GUI is ok)

--- unpack.py
+++ unpack.py
@@ -5,13 +5,14 @@ By:
 * Ken Shibata, Dec 2018
 '''
 
-import zipfile, os#, fleep
+import zipfile, os, shutil#, fleep
 
 class unpack():
     def __init__(self, pack_path):
         self.pack_path = pack_path
         self.pack_format = ''
         self.pack_compatible = None
+        self.unpack_dir_raw = 'unpacker_result_raw'
         self.unpack_dir = 'unpacker_result'
     def _find_format(self):
         pass
@@ -32,11 +33,14 @@ class unpack():
         self.pack_format = ['zip']
         if 'zip' in self.pack_format:
             with zipfile.ZipFile(self.pack_path, 'r') as zip_ref:
-                zip_ref.extractall(self.unpack_dir)
-        # old_wd = os.cwd()
-        # os.chdir(self.unpack_dir)
-        # TODO Move file contents of folder if there is only one folder inside unpacker_result.
-        # os.chdir(old_wd)
+                zip_ref.extractall(self.unpack_dir_raw)
+        # Check if there is only 1 dir, if 1, move dir contents to ../*
+        if len(list(os.listdir('./{}'.format(self.unpack_dir_raw)))) == 1:
+            # shutil.copytree(self.unpacker_dir_raw, self.unpack_dir)
+            shutil.rmtree('unpacker_result')
+            only_dir = list(os.listdir('./{}'.format(self.unpack_dir_raw)))[0]
+            shutil.copytree('unpacker_result_raw/{}'.format(only_dir), 'unpacker_result')
+        # TODO Move file contents of dir if there is only one dir inside unpacker_result
         print('Info  Done. Results in {}.'.format(self.unpack_dir))
 
 if __name__ == '__main__':
